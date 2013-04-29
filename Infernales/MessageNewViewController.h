626c1c80896e9795db3c459f12ed91f41c5424d0
Muenze <TRFC-Maverick@gmx.de>
2013-04-29 22:44:44 +0200

    drop down gibts

--- Infernales/MessageNewViewController.h
+++ Infernales/MessageNewViewController.h
@@ -7,12 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UIDropDownMenu.h"
+#import "ASIHTTPRequest.h"
+#import "JSON.h"
+#import <QuartzCore/QuartzCore.h>
 
-@interface MessageNewViewController : UIViewController <UIScrollViewDelegate>
+@interface MessageNewViewController : UIViewController <UIScrollViewDelegate, UIDropDownMenuDelegate> {
+    UIDropDownMenu *recieverMenu;
+}
 @property (retain, nonatomic) IBOutlet UIScrollView *svMyScrollView;
 @property (retain, nonatomic) IBOutlet UITextField *txtSubject;
 @property (retain, nonatomic) IBOutlet UITextView *txtMessage;
-@property (retain, nonatomic) IBOutlet UIPickerView *pickerReciever;
+@property (retain, nonatomic) IBOutlet UITextField *txtReciever;
+@property (strong, nonatomic) UIDropDownMenu *recieverMenu;
+@property (strong, nonatomic) NSArray *valueArray;
+@property (strong, nonatomic) NSArray *titleArray;
 
 
 
--- Infernales/MessageNewViewController.m
+++ Infernales/MessageNewViewController.m
@@ -14,7 +14,13 @@
 
 @implementation MessageNewViewController
 
-@synthesize svMyScrollView = _svMyScrollView;
+@synthesize svMyScrollView  = _svMyScrollView;
+@synthesize recieverMenu;
+@synthesize titleArray      = _titleArray;
+@synthesize valueArray      = _valueArray;
+@synthesize txtMessage      = _txtMessage;
+@synthesize txtReciever     = _txtReciever;
+@synthesize txtSubject      = _txtSubject;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,6 +34,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self fetchUsers];
+    
+    recieverMenu = [[UIDropDownMenu alloc] initWithIdentifier:@"recieverMenu"];
+    recieverMenu.ScaleToFitParent = TRUE;
+    recieverMenu.titleArray = _titleArray;
+    recieverMenu.valueArray = _valueArray;
+    [recieverMenu makeMenu:_txtReciever targetView:self.view];
+    recieverMenu.delegate = self;
+    
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -41,15 +57,41 @@
     [_svMyScrollView release];
     [_txtSubject release];
     [_txtMessage release];
-    [_pickerReciever release];
+    [_txtReciever release];
     [super dealloc];
 }
 - (void)viewDidUnload {
     [self setSvMyScrollView:nil];
     [self setTxtSubject:nil];
     [self setTxtMessage:nil];
-    [self setPickerReciever:nil];
+    [self setTxtReciever:nil];
     [super viewDidUnload];
+}
+
+#pragma mark userFunctions
+
+-(void)fetchUsers {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    NSString *urlString = [NSString stringWithFormat:@"http://www.infernales.de/portal/forum/collections.json.php?username=%@&password=%@&collection=alluser", [defaults objectForKey:@"username"], [defaults objectForKey:@"passwort"]];
+    NSDictionary *dic = [[NSString stringWithContentsOfURL:[NSURL URLWithString:urlString] encoding:NSUTF8StringEncoding error:nil] JSONValue];
+    _titleArray = [dic objectForKey:@"title"];
+    _valueArray = [dic objectForKey:@"value"];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+
+#pragma mark UIDropDownMenuDelegate functions
+
+- (void) DropDownMenuDidChange:(NSString *)identifier :(NSString *)ReturnValue{
+    
+    if ([identifier compare:@"recieverMenu"] == NSOrderedSame){
+        _txtReciever.text = ReturnValue;
+    }
 }
 
 @end
